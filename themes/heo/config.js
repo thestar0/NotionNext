e1a1f491d0e6072c551f13ea4c97e56ea0a5f36a
thestar0 <97114131+thestar0@users.noreply.github.com>
2025-06-13 04:39:14 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -41,12 +41,12 @@ const CONFIG = {
   ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about',
-  HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  HEO_INFO_CARD_URL1: 'https://github.com/thestar0',
+  HEO_INFO_CARD_ICON1: 'fab fa-github',
+  HEO_INFO_CARD_URL2: '/friends',
+  HEO_INFO_CARD_ICON2: 'fas fa-user',
+  HEO_INFO_CARD_URL3: '/about',
+  HEO_INFO_CARD_TEXT3: '关于我',
 
   // 用户技能图标
  HEO_GROUP_ICONS: [
